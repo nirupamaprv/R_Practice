c35d74c8ea3b43ae3ed83abd3d9b9aa2e3fb5f0b
Nirupama Puthur Venkataraman <nirupamaprv@gmail.com>
2018-02-18 16:04:50 -0600

    add: coursework exercise files

---
+++ Reddit_Factors.R
@@ -0,0 +1,33 @@
+getwd()
+reddit <- read.csv('reddit.csv')
+table(reddit$employment.status)
+
+str(reddit)
+levels(reddit$age.range)
+
+library(ggplot2)
+qplot(data=reddit, x=age.range)
+qplot(data=reddit, x=income.range)
+
+# Order the factor levels in the age.range variable in order to create
+# a graph with a natural order. Look up the documentation for
+# the factor function or read through the example in the Instructor Notes.
+
+# Once you're ready, try to write the code to order the levels of
+# the age.range variable.
+
+# Be sure you modify the variable in the data frame. That is modify reddit$age.range.
+# Don't create a new variable.
+
+# The levels of age.range should take on these values...
+
+#    "Under 18", "18-24", "25-34", "35-44", "45-54", "55-64", "65 or Above"
+
+# This exercise is ungraded. You can check your own work by using the Test Run
+# button. Your plot will appear there.
+
+# ENTER YOUR CODE BELOW THE LINE.
+# ================================================================================
+reddit$age.range <- ordered(reddit$age.range, levels = c("Under 18", "18-24", "25-34","35-44", "45-54","55-64","65 or Above"))
+reddit$age.range
+qplot(x=reddit$age.range)
